ee2f8c426ed1e6da4c6fbffb56338374737467e7
Tytus Planck <tytusplanck@gmail.com>
2017-09-10 17:21:27 -0400

    Added some CSV stuff.

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,12 @@
 # Assignment 1 - Data Mining
 # Tytus Planck and Kyle Rossman
+import csv
+data = []
+with open('income_tr.csv', 'rb') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in csvreader:
+        dataRow = []
+        for item in row:
+            dataRow.append(item)
+        data.append(dataRow)
+print data
