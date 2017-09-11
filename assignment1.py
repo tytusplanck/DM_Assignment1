c8932205e6f7462b1150418fb184d1783a471389
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2017-09-11 09:38:09 -0400

    Hopefully gave functions correct syntax LOL

--- assignment1.py
+++ assignment1.py
@@ -12,14 +12,13 @@ with open('income_tr.csv', 'rb') as csvfile:
         data.append(dataRow)
 print data[1][0]
 
-def defineK() {
+def defineK():
     # Asks User to define a value for K and then saves it.
     int x = 5
     return x
-}
 
 #Will break up the data into attributes and return a matrix of approximations for each attribute.
-def generateApproximationMatrix( k, data) {
+def generateApproximationMatrix( k, data):
     #This will call the function that will find all approximations for one instance. It will loop this so it can have an array of approximations for all instances.
     index = 0
     approximationMatrix = []
@@ -27,26 +26,21 @@ def generateApproximationMatrix( k, data) {
         approximationMatrix[index] = approximateOneInstance(data, index)
         index = index + 1
     return approximationMatrix #returns array of approximations for each
-}
 
 #Will Take one instance and find all of the approximations to the other instances
-def approximateOneInstance( data, currentInstance) {
-
+def approximateOneInstance( data, currentInstance):{
     instanceIndex = 0
     approximationArray = []     #Array of 520 floats containing the approximation to current instance for each object.
     while instanceIndex < data.__len__
         approximationArray[instanceIndex] = findRowApproximation(data[instanceIndex], data[currentIndex])
         instanceIndex = instanceIndex + 1
-    return approximationArray #Will return an array of the approximation of each instance to the current one.
-}
+    return approximationArray #Will return an array of the approximation of each instance to the current one
 
 #Takes one row of the table and finds it's approximation to the other.
-def findRowApproximation( row1, row2 ) {
+def findRowApproximation( row1, row2 ):
     smc = getSMCProximity(row1, row2)
 
-}
-
-def getSMCProximity (row1, row2) {
+def getSMCProximity (row1, row2):
     #Need to determine you can compare strings in python this way
     match = 0.0
     total = 0.0
@@ -103,16 +97,15 @@ def getSMCProximity (row1, row2) {
         total = total + 1
 
     return match / total #Calculates the SMC Proximation 
-}
 
-def getEuclidianProximation(row1, row2) {
-    
-    euc = 
+def getEuclidianProximation(row1, row2): 
+    euc = 0
     return math.sqrt(euc)
-}
+
 
 def main():
-    int k = defineK()
+    k = defineK()
+    generateApproximationMatrix(k, data)
 
 if __name__ == "__main__":
-    main(
+    main()
