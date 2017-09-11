9700969eaa95bbf7e87179be1a7eacb31a9bebc1
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2017-09-11 09:07:30 -0400

    The SMC proximity is now calculated.

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,7 @@
 # Assignment 1 - Data Mining
 # Tytus Planck and Kyle Rossman
 import csv
+import math
 data = []
 with open('income_tr.csv', 'rb') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -41,7 +42,73 @@ def approximateOneInstance( data, currentInstance) {
 
 #Takes one row of the table and finds it's approximation to the other.
 def findRowApproximation( row1, row2 ) {
+    smc = getSMCProximity(row1, row2)
+
+}
+
+def getSMCProximity (row1, row2) {
+    #Need to determine you can compare strings in python this way
+    match = 0.0
+    total = 0.0
+    #Finds match and total values for SMC algorithm
+    if (row1[2] == row2[2]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[4] == row2[4]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[6] == row2[6]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[7] == row2[7]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[8] == row2[8]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[8] == row2[8]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[9] == row2[9]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[10] == row2[10]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[14] == row2[14]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+    if (row1[15] == row2[15]):
+        match = match + 1
+        total = total + 1
+    else:
+        total = total + 1
+
+    return match / total #Calculates the SMC Proximation 
+}
+
+def getEuclidianProximation(row1, row2) {
     
+    euc = 
+    return math.sqrt(euc)
 }
 
 def main():
